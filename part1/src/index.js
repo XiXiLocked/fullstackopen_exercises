23ff3487f1730611f7929a2e9a3fff5057e309d7
奚骏毅 <xijunyi@littlelights.ai>
2020-06-28 18:36:15 +0800

    part1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,74 +3,76 @@ import ReactDOM from 'react-dom'
 
 
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const  Header =(props)=>{
-    return (
-      <>
-      <h1>{props.course}</h1>
-      </>
-    )
-  }
-
-  const Part = (props)=>{
-    return (
-      <>
-      <p>
-      {props.part} {props.exercises}
-    </p> 
+const  Header =(props)=>{
+  return (
+    <>
+    <h1>{props.course}</h1>
     </>
-    )
-  }
-  
-  const Content = (props) =>{
-    return (
-      <>
-      <Part part = {props.part1} exercises = {props.exercises1}/>
-      <Part part = {props.part2} exercises = {props.exercises2}/>
-      <Part part = {props.part3} exercises = {props.exercises3}/>
-      </>
-    )
-  
-  }
-  
-  const Total =(props) =>{
-    return (
-      <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-      </>
-    )
-    
-    
-  }
+  )
+}
 
-  // return (
-  //   <div>
-  //     <h1>{course}</h1>
-  //     <p>
-  //       {part1} {exercises1}
-  //     </p>
-  //     <p>
-  //       {part2} {exercises2}
-  //     </p>
-  //     <p>
-  //       {part3} {exercises3}
-  //     </p>
-  //     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  //   </div>
-  // )
+const Part = (props)=>{
+  return (
+    <>
+    <p>
+    {props.name} {props.exercises}
+  </p> 
+  </>
+  )
+}
+
+const Content = (props) =>{
+  return (
+    
+    <>
+        {
+      props.parts.map((p,i)=>
+        <Part name = {p.name} exercises = {p.exercises} key={i}/>)
+    }
+    </>
+  )
+
+}
+
+const Total =(props) =>{
+  let total = 0
+  for (let i=0;i<props.parts.length;++i)
+  {
+    total += props.parts[i].exercises;
+  }
+  return (
+    <>
+    <p>Number of exercises {total}</p>
+    </>
+  )
+  
+  
+}
+
+const App = () => {
+  const course = {
+    name : 'Half Stack application development',
+    parts :[
+    {
+    name :'Fundamentals of React', 
+    exercises : 10
+  }
+  ,{
+    name :'Using props to pass data',
+    exercises : 7
+  }
+  ,{
+    name:'State of a component',
+    exercises : 14
+  }]
+}
+
+
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2 = {part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total  exercises1={exercises1}  exercises2={exercises2}  exercises3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total  parts={course.parts} />
     </div>
   )
 }
